ce64140af4268ce6ead508f2ab591fb73d9b371e
kasonyang <me@kason.site>
2020-08-13 23:20:06 +0800

    feat: add commenter

---
+++ src/main/java/site/kason/kalang/intellij/KalangCommenter.java
@@ -0,0 +1,34 @@
+package site.kason.kalang.intellij;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author KasonYang
+ */
+public class KalangCommenter implements Commenter {
+    @Override
+    public @Nullable String getLineCommentPrefix() {
+        return "//";
+    }
+
+    @Override
+    public @Nullable String getBlockCommentPrefix() {
+        return "/*";
+    }
+
+    @Override
+    public @Nullable String getBlockCommentSuffix() {
+        return "*/";
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
